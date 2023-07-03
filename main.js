1c990fda5a07a5f0ae447e5103f57f7f05692967
kltransylvania <138440119+kltransylvania@users.noreply.github.com>
2023-07-03 10:50:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function show(){
+
+document.getElementById('nav').classlist.toggle('active');
+
+
+}
+
+
+let cookieModal = document.querySelector(".cookie-consent-modal")
+let cancelCookieBtn = document.querySelector(".btn.cancel")
+let acceptCookieBtn = document.querySelector(".btn.accept")
+
+cancelCookieBtn.addEventListener("click", function(){
+	cookieModal.classList.remove("active")
+	
+	})
+	
+	
+acceptCookieBtn.addEventListener("click", function(){
+	cookieModal.classList.remove("active")
+	localStorage.setItem("cookieAccepted", "yes")
+})
+setTimeout(function(){
+	let cookieAccepted = localStorage.getItem("cookieAccepted")
+	if(cookieAccepted !="yes"){
+		cookieModal.classList.add("active")
+	}
+	
+	
+},2000)
+*/
